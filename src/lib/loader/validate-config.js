ae8fee69d2ae10b03aa41deb8d2c0a7f056f76a7
bvtrots <bvtrots@gmail.com>
2026-04-05 10:39:30 +0300

    ✨ feat (loader): implement config validation
    - ✨ add `validateConfig` utility to verify current application settings.
    - ✨ implement `formatValidationError` for user-friendly error format.
    - ✨ add recommended actions for errors.
    - ✨ enhance error reporting technical AJV errors -> readable strings.

--- src/lib/loader/validate-config.js
+++ src/lib/loader/validate-config.js
@@ -41,6 +41,61 @@ const generateValidator = (key, schema) => {
   }
 };
 
+
+const formatValidationError = (err) => {
+  switch (err.keyword) {
+    case 'required':
+      return `Missing property: '${err.params.missingProperty}'`;
+
+    case 'additionalProperties':
+      return `Unknown property: '${err.params.additionalProperty}'`;
+
+    case 'type':
+      return `Expected type: '${err.params.type}'`;
+
+    case 'enum':
+      return `Allowed values: [${err.params.allowedValues.map(v => `'${v}'`).join(', ')}]`;
+
+    default:
+      return err.message || 'Validation error';
+  }
+};
+
+
+const validateConfig = (key, validator, currentConfig) => {
+  const config = currentConfig ? {...currentConfig} : {};
+  delete config.$schema;
+
+  const result = {ok: false, info: [], warnings: [], errors: []};
+
+  if (!validator) {
+    result.ok = false;
+    result.errors.push({
+       message: `Internal error: validator is not available.`,
+       recommendation: `Check the "${key}" validator status in the validators block in the report or reinstall the package: npm install bvtrots-dx@latest`,
+       meta: {isCritical: true}
+      });
+    return result;
+  }
+
+  const isValid = validator(config);
+  result.ok     = isValid;
+
+  if (!isValid) {
+    const errors = validator.errors || [];
+    result.errors = errors.map(err => ({
+      message: formatValidationError(err),
+      recommendation: `Ensure syntax errors in file or reinstall the package: npm install bvtrots-dx@latest`,
+      path: err.instancePath || 'root',
+      meta: {isCritical: true, type: 'validation'}
+    })) || [];
+  }
+
+  return result;
+};
+
 module.exports = {
-  generateValidator
+  generateValidator,
+  formatValidationError,
+  validateConfig
 }
