7946c1bf3f8392022cf1d426ea7fa6410c723a42
bvtrots <bvtrots@gmail.com>
2026-04-05 16:01:56 +0300

    🔧 chore (loader): remove initial prototype implementation
    - 🔧 clean up legacy loader code and temporary structures.
    - 🔧 streamline the module directory by removing unused trial files.

--- src/validator/validation-context.js
+++
@@ -1,9 +0,0 @@
-const loadConfig = require("../lib/loader");
-
-const { types: typesData, scopes: scopesData, settings } = loadConfig();
-
-const emojiMap = Object.fromEntries(Object.entries(typesData).map(([key, val]) => [key, val.emoji]));
-const scopesMap = Object.keys(scopesData);
-const emojiPattern = Object.values(emojiMap).join('|');
-
-module.exports = { typesData, scopesData, settings, emojiMap, emojiPattern, scopesMap };
